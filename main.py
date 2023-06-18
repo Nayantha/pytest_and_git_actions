d7e68d3c099890a2180f7a1d5896d8dee2642c4c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-18 15:05:14 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def dummy_func():
+    return 1
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import dummy_func
+
+def test_dummy_func():
+    assert dummy_func() == 1
